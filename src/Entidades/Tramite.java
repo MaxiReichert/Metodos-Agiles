9821bf705353b06705d83096e4b9c1f1b0cf1e83
[MaxiReichert] <[maximilianoreichert@gmail.com]>
2020-11-01 16:13:00 -0300

    Intial Commit

---
+++ src/Entidades/Tramite.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Maxi
+ */
+@Entity
+@Table(name = "tramite")
+@NamedQueries({
+    @NamedQuery(name = "Tramite.findAll", query = "SELECT t FROM Tramite t")})
+public class Tramite implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "fecha_reali")
+    @Temporal(TemporalType.DATE)
+    private Date fechaReali;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "numero_legajo")
+    private Usuario usuario;
+
+    public Tramite() {
+    }
+
+    public Tramite(Integer id) {
+        this.id = id;
+    }
+
+    public Tramite(Integer id, Date fechaReali) {
+        this.id = id;
+        this.fechaReali = fechaReali;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFechaReali() {
+        return fechaReali;
+    }
+
+    public void setFechaReali(Date fechaReali) {
+        this.fechaReali = fechaReali;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+}
