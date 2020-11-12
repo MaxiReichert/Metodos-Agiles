ef4568a7eeb6c215485f29051f92069d7f7c77ef
[MaxiReichert] <[maximilianoreichert@gmail.com]>
2020-11-12 19:01:50 -0300

    Agregue los import que me faltaron en titular y agregue libreria itext.

--- src/main/java/entidades/Titular.java
+++ src/main/java/entidades/Titular.java
@@ -7,10 +7,13 @@ package entidades;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
