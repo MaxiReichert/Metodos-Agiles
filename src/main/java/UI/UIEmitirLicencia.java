34cea697e7e8dc2993a4a3a16a001134f667f3be
josesei <joseignaciocarbone@gmail.com>
2020-11-18 19:44:27 -0300

    Agrego mock interfaz

--- src/main/java/UI/UIEmitirLicencia.java
+++ src/main/java/UI/UIEmitirLicencia.java
@@ -14,11 +14,15 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
 
 public class UIEmitirLicencia {
 
 	private JFrame frmAltaCliente;
-	private JTextField tfNroDNI;
+	private JTextField nroDocTextField;
+	private JTextField titularSeleccionadoTextField;
 
 	/**
 	 * Launch the application.
@@ -53,38 +57,91 @@ public class UIEmitirLicencia {
 		frmAltaCliente.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAltaCliente.getContentPane().setLayout(null);
 		
-		JComboBox comboTipoDNI = new JComboBox();
-		comboTipoDNI.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		comboTipoDNI.setBounds(97, 10, 164, 35);
+		JComboBox<String> comboTipoDNI = new JComboBox<String>();
+		comboTipoDNI.setModel(new DefaultComboBoxModel<String>(new String[] {"DNI"}));
+		comboTipoDNI.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		comboTipoDNI.setBounds(97, 79, 164, 35);
 		frmAltaCliente.getContentPane().add(comboTipoDNI);
 		
-		tfNroDNI = new JTextField();
-		tfNroDNI.setBounds(343, 11, 173, 34);
-		frmAltaCliente.getContentPane().add(tfNroDNI);
-		tfNroDNI.setColumns(10);
+		nroDocTextField = new JTextField();
+		nroDocTextField.setFont(new Font("Monospaced", Font.BOLD, 16));
+		nroDocTextField.setBounds(343, 79, 173, 34);
+		frmAltaCliente.getContentPane().add(nroDocTextField);
+		nroDocTextField.setColumns(10);
 		
-		JTextArea txtTipoDNI = new JTextArea();
-		txtTipoDNI.setFont(new Font("Monospaced", Font.PLAIN, 16));
-		txtTipoDNI.setBackground(UIManager.getColor("CheckBox.background"));
-		txtTipoDNI.setEditable(false);
-		txtTipoDNI.setText("TipoDNI");
-		txtTipoDNI.setBounds(10, 12, 77, 31);
-		frmAltaCliente.getContentPane().add(txtTipoDNI);
+		JTextArea txtTipoDOC = new JTextArea();
+		txtTipoDOC.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		txtTipoDOC.setBackground(UIManager.getColor("CheckBox.background"));
+		txtTipoDOC.setEditable(false);
+		txtTipoDOC.setText("TipoDOC");
+		txtTipoDOC.setBounds(10, 83, 77, 31);
+		frmAltaCliente.getContentPane().add(txtTipoDOC);
 		
-		JTextArea txtNroDNI = new JTextArea();
-		txtNroDNI.setBackground(UIManager.getColor("CheckBox.background"));
-		txtNroDNI.setFont(new Font("Monospaced", Font.PLAIN, 16));
-		txtNroDNI.setText("NroDNI");
-		txtNroDNI.setBounds(271, 12, 77, 34);
-		frmAltaCliente.getContentPane().add(txtNroDNI);
+		JTextArea txtNroDOC = new JTextArea();
+		txtNroDOC.setBackground(UIManager.getColor("CheckBox.background"));
+		txtNroDOC.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		txtNroDOC.setText("NroDOC");
+		txtNroDOC.setBounds(273, 83, 77, 34);
+		frmAltaCliente.getContentPane().add(txtNroDOC);
 		
-		JButton btnNewButton = new JButton("Seleccionar Titular");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton seleccionarTitularButton = new JButton("Seleccionar Titular");
+		seleccionarTitularButton.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		seleccionarTitularButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(539, 11, 184, 34);
-		frmAltaCliente.getContentPane().add(btnNewButton);
+		seleccionarTitularButton.setBounds(556, 79, 220, 34);
+		frmAltaCliente.getContentPane().add(seleccionarTitularButton);
+		
+		JLabel titularSeleccionadoLabel = new JLabel("Titular Seleccionado");
+		titularSeleccionadoLabel.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		titularSeleccionadoLabel.setBounds(79, 159, 227, 31);
+		frmAltaCliente.getContentPane().add(titularSeleccionadoLabel);
+		
+		titularSeleccionadoTextField = new JTextField();
+		titularSeleccionadoTextField.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		titularSeleccionadoTextField.setForeground(Color.RED);
+		titularSeleccionadoTextField.setEditable(false);
+		titularSeleccionadoTextField.setBounds(319, 157, 287, 34);
+		frmAltaCliente.getContentPane().add(titularSeleccionadoTextField);
+		titularSeleccionadoTextField.setColumns(10);
+		
+		JComboBox<String> comboTipoLicencia = new JComboBox<String>();
+		comboTipoLicencia.setModel(new DefaultComboBoxModel<String>(new String[] {"A", "B", "C", "D", "E", "F", "G"}));
+		comboTipoLicencia.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		comboTipoLicencia.setBounds(319, 258, 287, 35);
+		DefaultListCellRenderer centeredListRenderer = new DefaultListCellRenderer();
+	    centeredListRenderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER); // center-aligned items
+	    comboTipoLicencia.setRenderer(centeredListRenderer);
+		frmAltaCliente.getContentPane().add(comboTipoLicencia);
+		
+		JButton btnNewButton_1 = new JButton("Emitir Licencia");
+		btnNewButton_1.setForeground(Color.BLUE);
+		btnNewButton_1.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		btnNewButton_1.setBounds(449, 465, 220, 35);
+		frmAltaCliente.getContentPane().add(btnNewButton_1);
+		
+		JLabel tipoLicenciaLabel = new JLabel("Tipo Licencia");
+		tipoLicenciaLabel.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		tipoLicenciaLabel.setBounds(107, 260, 154, 31);
+		frmAltaCliente.getContentPane().add(tipoLicenciaLabel);
+		
+		JButton cancelarButton = new JButton("Cancelar");
+		cancelarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		cancelarButton.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		cancelarButton.setBounds(130, 465, 220, 35);
+		frmAltaCliente.getContentPane().add(cancelarButton);
+		
+		JLabel observacionesLabel = new JLabel("Observaciones");
+		observacionesLabel.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		observacionesLabel.setBounds(107, 337, 142, 31);
+		frmAltaCliente.getContentPane().add(observacionesLabel);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(319, 342, 285, 74);
+		frmAltaCliente.getContentPane().add(textArea);
 	}
 }
